5f7685ccefac13aaa8bd944de9b7bd40b97b3fe6
MaelREBOUX <m.reboux@rennesmetropole.fr>
2024-01-13 14:55:56 +0100

    Update create_database.sql
    character varying -> text

--- setup/create_database.sql
+++ setup/create_database.sql
@@ -8,16 +8,17 @@ CREATE DATABASE kerofis WITH OWNER=kerofis ENCODING=UTF8 ;
 
 
 -- create the main table
+DROP TABLE IF EXISTS public.kerofis ;
 CREATE TABLE public.kerofis (
     niv bigint NOT NULL,
     deiziad_degemer date,
     insee character varying(5),
-    kumun character varying(250),
-    stagadenn character varying(250),
-    lec_hanv character varying(250),
-    stumm_orin character varying(250),
-    rummad character varying(50),
-    stumm_dibab character varying(250),
+    kumun text,
+    stagadenn text,
+    lec_hanv text,
+    stumm_orin text,
+    rummad text,
+    stumm_dibab text,
     deiziad_restr date
 );
 
@@ -40,16 +41,17 @@ CREATE EXTENSION file_fdw ;
 -- then setup a virtual table linked on the csv data file
 CREATE SERVER kerofis_csv_import FOREIGN DATA WRAPPER file_fdw ; 
 
+DROP FOREIGN TABLE IF EXISTS public.kerofis_csv_import ;
 CREATE FOREIGN TABLE public.kerofis_csv_import
 (
-    stagadenn character varying(250),       -- stagadenn       / préfixe (FR)
-    lec_hanv character varying(250),        -- lec_hanv        / nom (FR)
-    kumun character varying(250),           -- kumun           / nom de la commune (BR)
+    stagadenn text,       -- stagadenn       / préfixe (FR)
+    lec_hanv text,        -- lec_hanv        / nom (FR)
+    kumun text,           -- kumun           / nom de la commune (BR)
     insee character varying(5),             -- insee           / code insee de la commune
-    rummad character varying(50),           -- rummad          / type de toponyme (BR)
-    dibab character varying(250),           -- stumm_dibab     / toponyme retenu (BR)
-    deiziad_degemer character varying(10),  -- deiziad_degemer / date validation ou publication  0000-00-00 ou dd/mm/yyyy
-    niv numeric(7,0)
+    rummad text,           -- rummad          / type de toponyme (BR)
+    dibab text,           -- stumm_dibab     / toponyme retenu (BR)
+    deiziad_degemer text,  -- deiziad_degemer / date validation ou publication  0000-00-00 ou dd/mm/yyyy
+    niv bigint
 )
 SERVER kerofis_csv_import
 OPTIONS (format 'csv', header 'true', delimiter ',', filename '/data/kerofis/kerofis_latest.csv');
